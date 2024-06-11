5374c365a4349051373214dbed88c613dc082763
Katlego <Katlegom9@gmail.com>
2024-06-11 23:20:40 +0200

    javascript referesher first commit

---
+++ variables.js
@@ -0,0 +1,28 @@
+/*Exercise 1: Variable Declaration*/
+
+const myName = 'Katlego';
+const myAge = 28;
+const isStudent = true;
+console.log(myName);
+console.log(myAge);
+console.log(isStudent);
+
+/*Exercise 2: Variable Reassignment*/
+
+const favoriteColour = 'Blue';
+console.log(favoriteColour);
+const favouriteColour = 'Burgundy';
+console.log(favouriteColour);
+
+/*Exercise 3: Arithmetic Operations*/
+
+const num1 = 14;
+const num2 = 7;
+const sum = num1 + num2;
+const diff = num1 - num2;
+const product = num1 * num2;
+const quotient = num1 / num2;
+console.log(sum);
+console.log(diff);
+console.log(product);
+console.log(quotient); 
