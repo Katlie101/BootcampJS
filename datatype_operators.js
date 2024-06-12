88c68655405e60a3845b27272465ff5c66ae36a4
Katlego <Katlegom9@gmail.com>
2024-06-12 03:02:07 +0200

    datatype operators commit

---
+++ datatype_operators.js
@@ -0,0 +1,86 @@
+/*Exercise 1: Numbers*/
+
+const normalNum = 20
+const floatNum = 10.0
+
+const newAdd = normalNum + 5; 
+const newSub = normalNum - 5;
+const newTimes = normalNum * 5;
+const newDiv = normalNum / 5;
+const newExpo = normalNum ** 5;
+const newPer = normalNum % 5;
+console.log(newAdd);
+console.log(newDiv);
+console.log(newTimes);
+console.log(newDiv);
+console.log(newExpo);
+console.log(newPer);
+
+const h = floatNum + 15
+const i = floatNum - 15
+const j = floatNum * 15
+const k = floatNum / 15
+const l = floatNum ** 15
+const m = floatNum % 15
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(k);
+console.log(l);
+console.log(m);
+
+/*Exercise 2: Boolean & Operators */
+
+const num3 = 25;
+const num4 = 50
+const equalTo = num3 == num4;
+const greaterThan = num3 > num4;
+const lessThan = num3 < num4;
+const notEqual = num3 != num4;
+//console.log(equalTo);
+//console.log(greaterThan);
+//console.log(lessThan);
+//console.log(notEqual);
+
+const x = 8;
+const y = 12;
+const greater = x > y;
+const lessOrEquals = x <= y;
+const equals = x == y;
+const notEquals = x != y;
+console.log(greater);
+console.log(lessOrEquals);
+console.log(equals);
+console.log(notEquals);
+
+const a = true;
+const b = false;
+const sameLetters = a == a && b == b ;
+const anyLetter = a == a || b == b;
+const notLetterA = !(a==b);
+console.log(sameLetters);
+console.log(anyLetter);
+console.log(notLetterA);
+
+let p = 10;
+p += 5;
+console.log(p);
+p -= 5;
+console.log(p);
+p *= 5;
+console.log(p);
+p /= 5;
+console.log(p);
+p %= 5;
+console.log(p);
+
+
+
+
+
+
+
+
+
+
+
