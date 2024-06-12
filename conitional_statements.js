e7fc578210130befb5d894e7b70b318abf1265c2
Katlego <Katlegom9@gmail.com>
2024-06-12 23:54:22 +0200

    loops commit

--- conitional_statements.js
+++ conitional_statements.js
@@ -34,3 +34,56 @@ switch (true) {
 }
 console.log(message)
 
+
+/*Exercise 2: Divisibility Check*/
+
+let number = 12; 
+
+if (number%2===0 && number%3===0){
+    show = "Divisble by both.";
+    console.log(show);
+} else if (number%2===0) {
+    show = "Divisable by 2.";
+    console.log(show);
+
+} else if (number%3===0){
+    show = "Divisable by 3.";
+    console.log(show);
+
+} else {
+    show = "Not divisable by 2 or 3.";
+    console.log(show);
+}
+
+/*Switch Expression*/
+
+let digit = 17;
+let divisible;
+
+switch (true) {
+    case digit%2===0 && digit%3===0:
+    divisible = "Divisible by both.";
+    break;
+    case digit%2===0:
+    divisible = "Divisible by 2.";
+    break;
+    case digit%3===0:
+    divisible = "Divisible by 3.";
+    break;
+    default:
+    divisible = "Not divisible by 2 or 3";
+}
+console.log(divisible);
+
+/*Exercise 3: For Loops*/
+
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+for (let i = 1; i<=20; i++) if (i%2===0) {
+    console.log(i);
+}
+
+
